f32ea9d4cd68b5a4ca76e24a4468cecd4f62559d
sbohra28 <shreybohra@gmail.com>
2023-07-09 20:59:42 -0400

    adding multiple dialogs

--- my-app/src/app/apikeys/columns.tsx
+++ my-app/src/app/apikeys/columns.tsx
@@ -2,26 +2,14 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { ApiKey } from "../page";
-import { MoreHorizontal } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
-
-import { EditKeyDialog } from "@/components/custom/edit-key-dialog";
+import { ApiKeyRowDropdown } from "@/components/custom/api-key-row-dropdown";
 
 const TRUNCATED_KEY_CHAR_COUNT = 4;
 
@@ -70,29 +58,7 @@ export const columns: ColumnDef<ApiKey>[] = [
     id: "actions",
     cell: ({ row }) => {
       const apiKey = row.original;
-
-      return (
-        <Dialog>
-          <EditKeyDialog apiKey={apiKey} />
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DialogTrigger asChild>
-                <DropdownMenuItem>Edit key</DropdownMenuItem>
-              </DialogTrigger>
-              <DropdownMenuItem>Rotate key</DropdownMenuItem>
-              <DropdownMenuItem>Delete key</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </Dialog>
-      );
+      return <ApiKeyRowDropdown apiKey={apiKey} />;
     },
   },
 ];
---
+++ my-app/src/components/custom/rotate-key-dialog.tsx
@@ -0,0 +1,45 @@
+import { ApiKey } from "@/app/page";
+import { Button } from "@/components/ui/button";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+type Props = {
+  apiKey: ApiKey;
+};
+
+export function RotateKeyDialog({ apiKey }: Props) {
+  const [name, setName] = useState<string>(apiKey.name);
+
+  return (
+    <DialogContent className="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>Rotate key</DialogTitle>
+        <DialogDescription>
+          Make changes to your key here. Click save when you&apos;re done.
+        </DialogDescription>
+      </DialogHeader>
+      <div className="grid gap-4 py-4">
+        <div className="grid grid-cols-4 items-center gap-4">
+          <Label htmlFor="name">Name</Label>
+          <Input
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="col-span-3"
+          />
+        </div>
+      </div>
+      <DialogFooter>
+        <Button type="submit">Save changes</Button>
+      </DialogFooter>
+    </DialogContent>
+  );
+}
