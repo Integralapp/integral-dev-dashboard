596bb02f297bc1002ff2da6c0f75d95c32b82f2b
sbohra28 <shreybohra@gmail.com>
2023-08-26 17:06:18 -0400

    revamp dev dashboard

---
+++ my-t3-app/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
---
+++ my-t3-app/src/server/api/routers/example.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import axios from "axios";
+
+type ApiKey = {
+  id: string;
+  createdAt: string;
+  lastUsed: string;
+  apiKey: string;
+  name?: string;
+};
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  apiKeys: publicProcedure
+    .input(z.object({ token: z.string(), applicationId: z.string() }))
+    .query(async ({ input }) => {
+      const { token, applicationId } = input;
+
+      const response = await axios.get(
+        "http://localhost:4000/dashboard/keys/list/production",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Integral-Application-Id": applicationId,
+          },
+        }
+      );
+      const apiKeys = response.data as Array<ApiKey>;
+      return apiKeys;
+    }),
+});
