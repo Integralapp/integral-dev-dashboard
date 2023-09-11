81186720d36f0639c90ee903d9f0242e3b5c908b
sbohra28 <shreybohra@gmail.com>
2023-09-10 22:54:58 -0400

    rotate key dialog ui

--- my-t3-app/src/@/components/rotate-key-dialog.tsx
+++ my-t3-app/src/@/components/rotate-key-dialog.tsx
@@ -8,9 +8,23 @@ import {
   DialogTitle,
 } from "./ui/dialog";
 import type { ApiKeyType } from "~/server/api/routers/example";
-import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
+enum ExpiresInTimes {
+  Now = "now",
+  OneHour = "1h",
+  TwentyFourHours = "24h",
+  ThreeDays = "3d",
+  SevenDays = "7d",
+}
 
 type Props = {
   apiKey: ApiKeyType;
@@ -23,7 +37,7 @@ export function RotateKeyDialog({
   setIsRotateKeyDialogOpen,
   isOpen,
 }: Props) {
-  const [name, setName] = useState<string>(apiKey.name ?? "");
+  const [expiresIn, setExpiresIn] = useState<ExpiresInTimes | null>(null);
 
   return (
     <Dialog open={isOpen}>
@@ -35,22 +49,42 @@ export function RotateKeyDialog({
         <DialogHeader>
           <DialogTitle>Rotate key</DialogTitle>
           <DialogDescription>
-            Make changes to your key here. Click save when you&apos;re done.
+            Choose an expiry time for your key.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="col-span-3"
-            />
-          </div>
+        <div className="flex flex-row items-center">
+          <Label htmlFor="expires in" className="pr-4">
+            Expiration
+          </Label>
+          <Select onValueChange={(e) => setExpiresIn(e as ExpiresInTimes)}>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select expiration" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value={ExpiresInTimes.Now}>now</SelectItem>
+              <SelectItem value={ExpiresInTimes.OneHour}>in 1 hour</SelectItem>
+              <SelectItem value={ExpiresInTimes.TwentyFourHours}>
+                in 24 hours
+              </SelectItem>
+              <SelectItem value={ExpiresInTimes.ThreeDays}>
+                in 3 days
+              </SelectItem>
+              <SelectItem value={ExpiresInTimes.SevenDays}>
+                in 7 days
+              </SelectItem>
+            </SelectContent>
+          </Select>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button
+            variant="secondary"
+            onClick={() => setIsRotateKeyDialogOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button type="submit" disabled={expiresIn === null}>
+            Save changes
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
