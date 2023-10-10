ac49523a33a434f27d71cc298f3313089f956042
sbohra28 <shreybohra@gmail.com>
2023-10-09 20:00:57 -0400

    fix

--- my-t3-app/src/pages/_app.tsx
+++ my-t3-app/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import "~/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
